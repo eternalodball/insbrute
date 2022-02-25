84386852bb51ff56ff5792c9168ba1a8b5113c1c
eternalodball <84136173+eternalodball@users.noreply.github.com>
2022-02-25 14:32:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install python3-pip -y
-sudo apt-get install google-chrome
+sudo apt-get install chromium -y
 pip3 install selenium
 pip3 install requests
 pip3 install chromedriver-py
