45dc825a7841373e8cb80f16e784d70e470da738
eternalodball <84136173+eternalodball@users.noreply.github.com>
2022-03-08 04:50:28 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,69 @@
 #!/bin/bash
 
+echo -e "\e[1m"
+
+#python
+
+echo "[+] Installing pip ..."
+echo ""
 sudo apt-get install python3-pip -y
+
+#chromium
+
+echo ""
+echo "[+] Do you want to install chromium browser (yes/no)"
+echo ""
+read -p ">> " option
+
+if [[ "$option" == "yes" ]]
+then
+echo ""
+echo "[+] installing chromium ..."
+echo ""
 sudo apt-get install chromium -y
+
+elif [[ $option == "no" ]]
+then
+echo ""
+echo "[+] Not installing chromium ..."
+
+else
+echo ""  
+echo "[!] No such option"
+echo ""
+fi
+
+# Selenium
+
+echo ""
+echo "[+] Installing selenium ..."
+
 pip3 install selenium
-pip3 install requests
+
+
+#chromedriver
+
+echo ""
+echo "[+] Do you want to install chromedriver (yes/no)"
+echo ""
+read -p ">> " option
+
+if [[ "$option" == "yes" ]]
+then
+echo ""
+echo "[+] installing chromedriver ..."
 pip3 install chromedriver-py
+
+elif [[ $option == "no" ]]
+then
+echo ""
+echo "[+] Not installing chromedriver ..."
+
+else
+echo ""  
+echo "[!] No such option"
+fi
+
+echo ""
+echo "[+] Dependencies installed ! , Happy hacking ;)"
+echo ""
